6bf4d985cc4a0cad96851033f438495a05a1c283
berta <berta.besteiro@gmail.com>
2016-06-29 13:03:05 +0200

    instalación, configuración y uso de json-loader

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    var comentarios= [
-      {autor: 'Oliver Khan', frase: 'Ultimamente veo más los abdominales de Cristiano Ronaldo que los pechos de mi mujer'},
-      {autor: 'Albert Einstein', frase: 'Dos cosas son infinitas: el universo y la estupidez humana; y yo no estoy seguro sobre el universo'}
-    ]
-    return (
-      <div>
-        {comentarios.map(comentario => <div><h1>{comentario.autor}</h1><p>{comentario.frase}</p></div>)}
-      </div>
+    var cervezas = require('./cervezas.json')
+   return (
+     <div>
+       <h1>Mi lista de cervezas</h1>
+       {cervezas.map(cerveza => <div><h2>{cerveza.Nombre}</h2><p>{cerveza.Envase}</p></div>)}
+     </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
+    },
+    { 
+      test: /\.json$/, 
+      loader: 'json-loader'
     }]
   }
 };
