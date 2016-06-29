27401e1de164c56fc5df7092e60155155a8386b8
berta <berta.besteiro@gmail.com>
2016-06-29 13:00:35 +0200

    Primeros pasos con react

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+    var comentarios= [
+      {autor: 'Oliver Khan', frase: 'Ultimamente veo más los abdominales de Cristiano Ronaldo que los pechos de mi mujer'},
+      {autor: 'Albert Einstein', frase: 'Dos cosas son infinitas: el universo y la estupidez humana; y yo no estoy seguro sobre el universo'}
+    ]
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        {comentarios.map(comentario => <div><h1>{comentario.autor}</h1><p>{comentario.frase}</p></div>)}
+      </div>
     );
   }
 }
